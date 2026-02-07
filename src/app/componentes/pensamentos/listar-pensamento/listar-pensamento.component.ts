3e334d71466474f7949f2eb18ff308db148d5f9e
Daniel Gutierres <daniel113388@gmail.com>
2026-02-06 22:45:58 -0300

    feat: add search functionality to ListarPensamentoComponent

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -13,6 +13,7 @@ export class ListarPensamentoComponent implements OnInit {
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
 
+  filtro: string = '';
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
